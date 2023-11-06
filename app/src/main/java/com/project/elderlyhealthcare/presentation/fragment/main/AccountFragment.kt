7710ae77e9a816d663495b42645606c80db6fce1
SONNT-PC\kha.nvu <kha.nvu@aris-vn.com>
2023-11-06 17:53:42 +0700

    commit 061123

--- app/src/main/java/com/project/elderlyhealthcare/presentation/fragment/main/AccountFragment.kt
+++ app/src/main/java/com/project/elderlyhealthcare/presentation/fragment/main/AccountFragment.kt
@@ -1,60 +1,38 @@
 package com.project.elderlyhealthcare.presentation.fragment.main
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
+import android.content.Intent
 import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.project.elderlyhealthcare.BR
 import com.project.elderlyhealthcare.R
+import com.project.elderlyhealthcare.databinding.FragmentAccountBinding
+import com.project.elderlyhealthcare.presentation.activity.NotLoginActivity
+import com.project.elderlyhealthcare.presentation.fragment.base.BaseFragment
+import com.project.elderlyhealthcare.presentation.fragment.not_login.LoginFragmentDirections
+import com.project.elderlyhealthcare.presentation.viewmodels.main.AccountViewModel
+import com.project.elderlyhealthcare.utils.SingleClickListener
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [AccountFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class AccountFragment : Fragment() {
-	// TODO: Rename and change types of parameters
-	private var param1: String? = null
-	private var param2: String? = null
+class AccountFragment : BaseFragment<AccountViewModel, FragmentAccountBinding>(R.layout.fragment_account) {
+	override fun variableId(): Int = BR.accountViewModel
 
-	override fun onCreate(savedInstanceState: Bundle?) {
-		super.onCreate(savedInstanceState)
-		arguments?.let {
-			param1 = it.getString(ARG_PARAM1)
-			param2 = it.getString(ARG_PARAM2)
+	override fun createViewModel(): Lazy<AccountViewModel> = viewModels()
+
+	override fun bindView(view: View): FragmentAccountBinding {
+		return FragmentAccountBinding.bind(view)
+	}
+
+	override fun init() {
+		super.init()
+		binding.apply {
+			accountLayoutLogout.setOnClickListener(object : SingleClickListener(){
+				override fun onSingleClick(v: View) {
+					startActivity(Intent (requireContext(), NotLoginActivity :: class.java))
+					activity?.finish()
+				}
+			})
 		}
 	}
 
-	override fun onCreateView(
-		inflater: LayoutInflater, container: ViewGroup?,
-		savedInstanceState: Bundle?
-	): View? {
-		// Inflate the layout for this fragment
-		return inflater.inflate(R.layout.fragment_account, container, false)
-	}
-
-	companion object {
-		/**
-		 * Use this factory method to create a new instance of
-		 * this fragment using the provided parameters.
-		 *
-		 * @param param1 Parameter 1.
-		 * @param param2 Parameter 2.
-		 * @return A new instance of fragment AccountFragment.
-		 */
-		// TODO: Rename and change types and number of parameters
-		@JvmStatic
-		fun newInstance(param1: String, param2: String) =
-			AccountFragment().apply {
-				arguments = Bundle().apply {
-					putString(ARG_PARAM1, param1)
-					putString(ARG_PARAM2, param2)
-				}
-			}
-	}
 }
---
+++ app/src/main/java/com/project/elderlyhealthcare/presentation/fragment/main/event/EventFragment.kt
@@ -0,0 +1,48 @@
+package com.project.elderlyhealthcare.presentation.fragment.main.event
+
+import android.content.Intent
+import android.view.View
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.project.elderlyhealthcare.BR
+import com.project.elderlyhealthcare.R
+import com.project.elderlyhealthcare.databinding.FragmentEventBinding
+import com.project.elderlyhealthcare.presentation.activity.NotLoginActivity
+import com.project.elderlyhealthcare.presentation.fragment.base.BaseFragment
+import com.project.elderlyhealthcare.presentation.viewmodels.main.EventViewModel
+import com.project.elderlyhealthcare.utils.SingleClickListener
+
+
+class EventFragment : BaseFragment<EventViewModel, FragmentEventBinding>(R.layout.fragment_event) {
+	override fun variableId(): Int = BR.eventViewModel
+
+	override fun createViewModel(): Lazy<EventViewModel> = viewModels()
+
+	override fun bindView(view: View): FragmentEventBinding {
+		return FragmentEventBinding.bind(view)
+	}
+
+	override fun init() {
+		super.init()
+		binding.apply {
+			eventLayoutExerciseEvent.setOnClickListener(object : SingleClickListener(){
+				override fun onSingleClick(v: View) {
+					findNavController().navigate(EventFragmentDirections.actionEventFragmentToExerciseEventFragment())
+				}
+			})
+
+			eventLayoutMedicineEvent.setOnClickListener(object : SingleClickListener(){
+				override fun onSingleClick(v: View) {
+					findNavController().navigate(EventFragmentDirections.actionEventFragmentToMedicineEventFragment())
+				}
+			})
+
+			eventLayoutReExaminationEvent.setOnClickListener(object : SingleClickListener(){
+				override fun onSingleClick(v: View) {
+					findNavController().navigate(EventFragmentDirections.actionEventFragmentToReExaminationEventFragment())
+				}
+			})
+		}
+	}
+
+}
