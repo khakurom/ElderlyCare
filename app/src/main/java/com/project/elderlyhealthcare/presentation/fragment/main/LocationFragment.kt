fb4abd2b0e8f10b28b22ece3cf744d1764de4a9b
KHA <ngovukha54@gmail.com>
2023-12-19 23:25:16 +0700

    commit 191223-2

--- app/src/main/java/com/project/elderlyhealthcare/presentation/fragment/main/LocationFragment.kt
+++ app/src/main/java/com/project/elderlyhealthcare/presentation/fragment/main/LocationFragment.kt
@@ -5,18 +5,36 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
 import com.project.elderlyhealthcare.R
 import dagger.hilt.android.AndroidEntryPoint
+import hilt_aggregated_deps._com_project_elderlyhealthcare_presentation_activity_NotLoginActivity_GeneratedInjector
 
 @AndroidEntryPoint
-class LocationFragment : Fragment() {
-
+class LocationFragment : Fragment() , OnMapReadyCallback{
+	private lateinit var mMap: GoogleMap
 
 	override fun onCreateView(
 		inflater: LayoutInflater, container: ViewGroup?,
 		savedInstanceState: Bundle?
 	): View? {
-		return inflater.inflate(R.layout.fragment_location, container, false)
+		val view = inflater.inflate(R.layout.fragment_location, container, false)
+
+		// Obtain the SupportMapFragment and get notified when the map is ready to be used.
+		val mapFragment =
+			childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+		mapFragment.getMapAsync(this)
+		return view
+	}
+
+	override fun onMapReady(googleMap: GoogleMap) {
+		mMap = googleMap
+		val hoChiMinhCity = LatLng(10.7769, 106.7009)
+		mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(hoChiMinhCity, 12f))
 	}
 
 
